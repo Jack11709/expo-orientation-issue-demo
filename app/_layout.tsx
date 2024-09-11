43edcf51f19d503a89f839c54a140445b74e93b2
May, Jack <jack.may@accenture.com>
2024-09-11 13:18:02 +0100

    demo created

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,11 +4,21 @@ import { Stack } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
 import { useEffect } from 'react'
 import 'react-native-reanimated'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import { useColorScheme } from '@/hooks/useColorScheme'
+import { Text, View } from 'react-native'
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
+
+const HeaderTitle = () => {
+  return (
+    <View style={{ backgroundColor: '#FF0000' }}>
+      <Text>Header Title</Text>
+    </View>
+  )
+}
 
 export default function RootLayout() {
   const colorScheme = useColorScheme()
@@ -27,11 +37,18 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <SafeAreaProvider
+      initialMetrics={{
+        frame: { x: 0, y: 0, width: 0, height: 0 },
+        insets: { top: 0, left: 0, right: 0, bottom: 0 }
+      }}
+    >
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack screenOptions={{ headerTitle: () => <HeaderTitle /> }}>
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </ThemeProvider>
+    </SafeAreaProvider>
   )
 }
